07c62447f695be64ded43fdba6b1651c69d30cbd
unknown <shubham.r0098@gmail.com>
2024-10-22 22:20:22 +0530

    Adding V1-EC2.tf file

---
+++ terraform-code/V1-EC2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-04a37924ffe27da53"
+  instance_type = "t2.micro"
+  security_groups = [ "demo-sg" ]
+}
