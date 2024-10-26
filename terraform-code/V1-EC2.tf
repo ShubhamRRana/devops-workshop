f4b3452b85033f4df28d5aed6a0ae35510d3de19
Shubham Rana <96970537+ShubhamRRana@users.noreply.github.com>
2024-10-26 13:00:31 +0530

    Update V1-EC2.tf

--- terraform-code/V1-EC2.tf
+++ terraform-code/V1-EC2.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+    ami = "ami-04a37924ffe27da53"
+    instance_type = "t2.micro
 }
